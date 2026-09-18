44596a5eac4dfbe9a70d6812579d12113c1c0482
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-18 10:13:59 +0530

    fix: return loading state in protected route

--- client/vite-project/src/components/ProtectedRoute.jsx
+++ client/vite-project/src/components/ProtectedRoute.jsx
@@ -1,28 +1,18 @@
-import React from 'react'
-import { useAuth } from '../context/AuthContext'
 import { Navigate } from 'react-router-dom'
-
+import { useAuth } from '../context/AuthContext'
 
 function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth()
 
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
 
+  if (!user) {
+    return <Navigate to='/login' replace />
+  }
 
-    const { user , loading } = useAuth()
-
-
-    if(loading){
-        <h1>Loading...</h1>
-    }
-
-
-
-    if (!user) {
-        return <Navigate to='/login' />
-    }
-
-
-
-    return children
+  return children
 }
 
 export default ProtectedRoute
