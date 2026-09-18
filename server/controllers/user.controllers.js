781c6e5b4dcb0ea67c3ed32ec9ac2bd104d99401
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-18 10:12:59 +0530

    fix: allow jwt auth cookie on localhost

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -6,7 +6,9 @@ import { genToken } from "../utils/generateToken.js"
 
 const cookiesOptions = {
     httpOnly: true,
-    secure: true
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 10 * 24 * 60 * 60 * 1000
 }
 
 export const resgiterUser = async (req, res) => {
