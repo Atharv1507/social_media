a522030137d0600e334e3654a49730ec56322e51
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-23 10:21:30 +0530

    enable follow and unfollow routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,27 +1,22 @@
 import express from 'express'
-import { followUser, getUser, getUserProfile, loginUser, resgiterUser } from '../controllers/user.controllers.js'
+import {
+    followUser,
+    getUser,
+    getUserProfile,
+    loginUser,
+    resgiterUser,
+    unFollowUser
+} from '../controllers/user.controllers.js'
 import { isAuthenticated } from '../middlewares/authMiddleware.js'
-
-
 
 const userRoutes = express.Router()
 
-// register
+userRoutes.post('/register', resgiterUser)
+userRoutes.post('/login', loginUser)
+userRoutes.get('/me', isAuthenticated, getUser)
+userRoutes.get('/profile/:username', isAuthenticated, getUserProfile)
 
-userRoutes.post('/register' , resgiterUser)
-
-userRoutes.post('/login' , loginUser)
-
-userRoutes.get('/me' , isAuthenticated , getUser)
-
-userRoutes.get('/profile/:username' , isAuthenticated , getUserProfile)
-
-// userRoutes.post('/follow/:id' , isAuthenticated , followUser)
-
-
-// userRoutes.get('/logout')
-
-
-
+userRoutes.post('/follow/:id', isAuthenticated, followUser)
+userRoutes.post('/unfollow/:id', isAuthenticated, unFollowUser)
 
 export default userRoutes
